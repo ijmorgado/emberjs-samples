3924e844c96ca811d9244838887ead8e22fed1a3
ijmorgado <i.jimenez.morgado@gmail.com>
2013-04-16 00:25:05 -0500

    Removed folder and files of development environment

--- sample1/components/ember-data/packages/ember-data/lib/core.js
+++
@@ -1,4 +0,0 @@
-window.DS = Ember.Namespace.create({
-  // this one goes past 11
-  CURRENT_API_REVISION: 12
-});
--- sample1/components/ember-data/packages/ember-data/lib/system/model.js
+++
@@ -1,3 +0,0 @@
-require("ember-data/system/model/model");
-require("ember-data/system/model/states");
-require("ember-data/system/model/attributes");
